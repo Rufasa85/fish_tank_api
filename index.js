badbe182f4e30232691bb5a2b6b01e782f9838c1
Joe Rehfuss <joe.rehfuss@gmail.com>
2020-08-13 13:35:19 -0700

    adding deploy config

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ var db = require('./models');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app.use(cors({
+//   origin:["http://localhost:3000"],
+//   credentials:true
+// }))
 app.use(cors({
-  origin:["http://localhost:3000"],
+  origin:["https://fish-tank-react.herokuapp.com"],
   credentials:true
 }))
 
